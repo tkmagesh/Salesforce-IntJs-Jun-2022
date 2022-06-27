bd13b5d2a491b11314381c91cbf6c5b7cd0aa57f
Magesh Kuppan <tkmagesh77@gmail.com>
2022-06-28 04:51:50 +0530

    add function - complete solution

--- 01-language/calculator.js
+++ 01-language/calculator.js
@@ -39,6 +39,7 @@ function add(){
 } 
 */
 
+/* 
 function add(){
     function parseArg(n){
         if (Array.isArray(n)) {
@@ -56,7 +57,32 @@ function add(){
         result += parseArg(arguments[i]);
     }
     return result;
-}
+} 
+*/
+
+/* 
+function add(){
+    function parseArg(n){
+        if (Array.isArray(n)) return add.apply(this, n); // array 'n' is converted as an argument list to the add function
+        if (typeof n === 'function') return parseArg(n());
+        return isNaN(n) ? 0 : parseInt(n);
+    }
+    var result = 0;
+    for (var i = 0; i < arguments.length; i++){
+        result += parseArg(arguments[i]);
+    }
+    return result;
+} 
+*/
+
+function add(){
+    function parseArg(n){
+        if (Array.isArray(n)) return add.apply(this, n); // array 'n' is converted as an argument list to the add function
+        if (typeof n === 'function') return parseArg(n());
+        return isNaN(n) ? 0 : parseInt(n);
+    }
+    return arguments.length <= 1 ? parseArg(arguments[0]) : parseArg(arguments[0]) + add([].slice.call(arguments, 1));
+} 
 
 
 //commonJS module
