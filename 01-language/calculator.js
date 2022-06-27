bd884a09a5a02a4d4306b7d9b8761ec544bb0664
Magesh Kuppan <tkmagesh77@gmail.com>
2022-06-28 03:14:05 +0530

    add function - version 1.0

--- 01-language/calculator.js
+++ 01-language/calculator.js
@@ -1,5 +1,63 @@
+/* 
 function add(x,y){
-    return x + y;
+    var x1 = isNaN(x) ? 0 : parseInt(x);
+    var y1 = isNaN(y) ? 0 : parseInt(y);
+    return x1 + y1;
+} 
+*/
+
+/* 
+function add(x,y){
+    function parseArg(n){
+        return isNaN(n) ? 0 : parseInt(n);
+    }
+    return parseArg(x) + parseArg(y);
+} 
+*/
+
+/* 
+function add(x,y){
+    function parseArg(n){
+        if (typeof n === 'function') return n();
+        return isNaN(n) ? 0 : parseInt(n);
+    }
+    return parseArg(x) + parseArg(y);
+} 
+*/
+
+/* 
+function add(){
+    function parseArg(n){
+        if (typeof n === 'function') return n();
+        return isNaN(n) ? 0 : parseInt(n);
+    }
+    var result = 0;
+    for (var i = 0; i < arguments.length; i++){
+        result += parseArg(arguments[i])
+    }
+    return result;
+} 
+*/
+
+function add(){
+    function parseArg(n){
+        if (Array.isArray(n)) {
+            var result = 0;
+            for (var i = 0; i < n.length; i++){
+                result += parseArg(n[i]);
+            }
+            return result;
+        }
+        if (typeof n === 'function') return parseArg(n());
+        return isNaN(n) ? 0 : parseInt(n);
+    }
+    var result = 0;
+    for (var i = 0; i < arguments.length; i++){
+        result += parseArg(arguments[i]);
+    }
+    return result;
 }
 
+
+//commonJS module
 module.exports = add;
--- 01-language/calculator.test.js
+++ 01-language/calculator.test.js
@@ -7,46 +7,61 @@ test('Adding 2 numbers', function(){
 });
 
 test("Adding numbers in string format", function(){
-    var actualResult = add(10,"20")                                                    //=> 30
+    var actualResult = add(10,"20")                                                   
     expect(actualResult).toBe(30)
 })
+
 test("Adding a non-number string", function(){
-    var actualResult = add(10,"abc")                                                   //=> 10
+    var actualResult = add(10,"abc")                                                  
     expect(actualResult).toBe(10)
 })
+
 test("Adding array of numbers", function(){
-    var actualResult = add([10,20],[30,40])                                            //=> 100
+    var actualResult = add([10,20],[30,40])                                           
     expect(actualResult).toBe(100)
 })
+
 test("Adding array with numbers in string format", function(){
-    var actualResult = add([10,20],[30,"40"])                                          //=> 100
+    var actualResult = add([10,20],[30,"40"])                                         
     expect(actualResult).toBe(100)
 })
+
 test("Adding array of number in string format", function(){
-    var actualResult = add([10,20],[30,"abc"])                                         //=> 60
+    var actualResult = add([10,20],[30,"abc"])                                        
     expect(actualResult).toBe(60)
 })
+
 test("Adding nested array of numbers and strings", function(){
-    var actualResult = add([10,20],[30,[40,"abc"]])                                    //=> 100
+    var actualResult = add([10,20],[30,[40,"abc"]])                                   
     expect(actualResult).toBe(100)
 })
+
 test("Adding functions returning numbers", function(){
-    var actualResult = add(function(){return 10;}, function(){ return 20;})            //=> 30
+    var actualResult = add(function(){return 10;}, function(){ return 20;})           
     expect(actualResult).toBe(30)
 })
+
 test("Adding functions returning array of numbers", function(){
-    var actualResult = add(function(){return [10,20];}, function(){ return [30,40];})  //=> 100
+    var actualResult = add(function(){return [10,20];}, function(){ return [30,40];}) 
     expect(actualResult).toBe(100)
 })
+
 test("Adding one number", function(){
-    var actualResult = add(10)                                                         //=> 10
+    var actualResult = add(10)                                                        
     expect(actualResult).toBe(10)
 })
+
 test("Add with no arguments", function(){
-    var actualResult = add()                                                           //=> 0
+    var actualResult = add()                                                          
     expect(actualResult).toBe(0)
 })
+
+test("Add varying number of numbers", function(){
+    var actualResult = add(10,20,30,40,50)                                                          
+    expect(actualResult).toBe(150)
+})
+
 test("Adding array of functions returning array of numbers", function(){
-    var actualResult = add([function(){return [10,20];}, function(){ return [30,40];}]) //=> 100
+    var actualResult = add([function(){return [10,20];}, function(){ return [30,40];}])
     expect(actualResult).toBe(100)
 })
