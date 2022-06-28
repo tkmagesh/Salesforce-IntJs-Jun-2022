cf4e21775b799a4da43b0637c1a69b43ef8498fd
Magesh Kuppan <tkmagesh77@gmail.com>
2022-06-28 23:33:12 +0530

    spinner - solution

--- 02-closure/spinner.js
+++ 02-closure/spinner.js
@@ -32,4 +32,45 @@ IMPORTANT:
 
 function fn(){
     console.log('fn invoked')
-} */
+} 
+
+*/
+
+var spinner = (function (){
+    var counter = 0;
+
+    function up(){
+        return ++counter;
+    }
+
+    function down(){
+        return --counter;
+    }
+
+    return {
+        up : up,
+        down : down
+    };
+
+})()
+
+function spinnerFactory(){
+    var counter = 0;
+
+    function up(){
+        return ++counter;
+    }
+
+    function down(){
+        return --counter;
+    }
+
+    return {
+        up : up,
+        down : down
+    };
+
+}
+
+var s1 = spinnerFactory();
+var s2 = spinnerFactory();
