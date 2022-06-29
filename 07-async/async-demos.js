50d97f3a84b0d8d902da8a1c55f8cda4999cc319
Magesh Kuppan <tkmagesh77@gmail.com>
2022-06-30 04:05:51 +0530

    promises - complete

--- 07-async/async-demos.js
+++ 07-async/async-demos.js
@@ -48,7 +48,7 @@
         return promise;
     }
 
-    window['addAsyncPromise'] = addAsyncPromise;
+    //window['addAsyncPromise'] = addAsyncPromise;
 
 
     /* 
@@ -59,5 +59,114 @@
     }) 
     */
 
+    //Promise chaining
+    /* 
+        console.log(`[@client] invoking the service`)
+        var p = addAsyncPromise(100,200)
+        var p2 = p.then(function(result){
+            console.log(`[@client] result = ${result}`);
+            return new Promise(function(resolveFn, rejectFn){
+                setTimeout(function(){
+                    var doubleResult = result * 2;
+                    resolveFn(doubleResult);
+                },5000)
+            })
+            
+        })
+        var p3 = p2.then(function(doubleResult){
+            console.log(`doubleResult = ${doubleResult}`)
+            return 'dummy'
+        });
+        var p4 = p3.then(function(s){
+            console.log(s);
+        })
 
+    */
+
+   
+
+    /* 
+    function addAsyncPromiseClient(x,y){
+        console.log(`[@client] invoking the service`)
+        var p = addAsyncPromise(x,y)
+        p.then(function(result){
+            console.log(`[@client] result = ${result}`);
+        }); 
+    } 
+    */
+
+    async function addAsyncPromiseClient(x,y){
+        console.log(`[@client] invoking the service`)
+        let result = await addAsyncPromise(x,y)
+        console.log(`[@client] result = ${result}`);
+        return result * 2;
+    }
+
+    window['addAsyncPromiseClient'] = addAsyncPromiseClient;
+
+   /*  
+   async function batchAddAsyncPromiseClient(){
+        let result1 = await addAsyncPromise(10,20);
+        console.log(`result1 = ${result1}`);
+
+        let result2 = await addAsyncPromise(100,200);
+        console.log(`result2 = ${result2}`);
+
+        let result3 = await addAsyncPromise(1000,2000);
+        console.log(`result3 = ${result3}`);
+    } 
+    */
+
+   /* 
+    function batchAddAsyncPromiseClient(){
+        return addAsyncPromise(10,20)
+            .then(function(result1){
+                console.log(`result1 = ${result1}`);
+                return addAsyncPromise(100,200)
+                    .then(function(result2){
+                        console.log(`result2 = ${result2}`);
+                        return addAsyncPromise(1000,2000)
+                            .then(function(result3){
+                                console.log(`result3 = ${result3}`);
+                            })
+                    })
+            })        
+    } 
+    */
+
+    /* 
+    function batchAddAsyncPromiseClient(){
+        addAsyncPromise(10,20)
+            .then(function(result1){
+                console.log(`result1 = ${result1}`)
+            })
+
+        addAsyncPromise(100,200)
+            .then(function(result2){
+                console.log(`result2 = ${result2}`)
+            })
+
+        addAsyncPromise(1000,2000)
+            .then(function(result3){
+                console.log(`result3 = ${result3}`)
+            })
+        
+    }  
+    */
+
+    function batchAddAsyncPromiseClient(){
+        var p1 = addAsyncPromise(10,20);
+        var p2 = addAsyncPromise(100,200);
+        var p3 = addAsyncPromise(1000,2000);
+
+        Promise.all([p1, p2, p3])
+            .then(function(results){
+                console.log(`result1 = ${results[0]}`)
+                console.log(`result2 = ${results[1]}`)
+                console.log(`result3 = ${results[2]}`)
+            })
+            
+    } 
+
+    window['batchAddAsyncPromiseClient'] = batchAddAsyncPromiseClient;
 })();
