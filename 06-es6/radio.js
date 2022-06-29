e1e5cb00cf3ff3bf2e49bc7eb20d41871f600dc1
Magesh Kuppan <tkmagesh77@gmail.com>
2022-06-30 01:07:46 +0530

    radio.js implementation

--- 06-es6/radio.js
+++ 06-es6/radio.js
@@ -1,4 +1,44 @@
-radio
+
+let radio = (function(){
+    let _radios = {};
+
+    function radio(evtName){
+        _radios[evtName] = _radios[evtName] || new Radio(evtName);
+        return _radios[evtName];
+    }
+
+    class Radio{
+
+        #subscribers = [];
+        evtName = '';
+
+        constructor(evtName){
+            this.evtName = evtName;
+        }
+
+        subscribe(subscriptionFn){
+            if (typeof subscriptionFn !== 'function')
+                throw new Error('subscription should be a function')
+            this.#subscribers.push(subscriptionFn);
+            return this;
+        }
+
+        unsubscribe(subscriptionFn){
+            this.#subscribers = this.#subscribers.filter(subscription => subscription !== subscriptionFn)
+            return this;
+        }
+
+        broadcast(...args){
+            this.#subscribers.forEach(subscriptionFn => subscriptionFn(...args))
+            return this;
+        }
+
+    }
+
+    return radio;
+})();
+
+
 
 //subscription
 let subscription1 = (...args) => {
